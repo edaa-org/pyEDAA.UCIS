614c1ff0191ff8a3d70528272ba1cddbb60d916b
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-01-15 22:48:20 +0100

    Added agreed file header.
    Email from Michal Pacula <michalp@aldec.com> on Wednesday, October 13, 2021 4:46 PM.

--- pyEDAA/UCIS/CLI/UCIS.py
+++ pyEDAA/UCIS/CLI/UCIS.py
@@ -1,8 +1,44 @@
-# Usage:
-# acdb2xml -i aggregate.acdb -o ucdb.xml
-# python ucdb2cobertura.py -i ucdb.xml -o cobertura.xml
+# ==================================================================================================================== #
+#               _____ ____    _        _     _   _  ____ ___ ____                                                      #
+#   _ __  _   _| ____|  _ \  / \      / \   | | | |/ ___|_ _/ ___|                                                     #
+#  | '_ \| | | |  _| | | | |/ _ \    / _ \  | | | | |    | |\___ \                                                     #
+#  | |_) | |_| | |___| |_| / ___ \  / ___ \ | |_| | |___ | | ___) |                                                    #
+#  | .__/ \__, |_____|____/_/   \_\/_/   \_(_)___/ \____|___|____/                                                     #
+#  |_|    |___/                                                                                                        #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Artur Porebski (Aldec Inc.)                                                                                        #
+#   Michal Pacula  (Aldec Inc.)                                                                                        #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2021-2022 Electronic Design Automation Abstraction (EDA²)                                                  #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#          http://www.apache.org/licenses/LICENSE-2.0                                                                  #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
+#
+"""
+Tools to extract data from UCIS datafiles.
 
+.. topic:: Usage
 
+.. code-block::
+
+   acdb2xml -i aggregate.acdb -o ucdb.xml
+   python ucdb2cobertura.py -i ucdb.xml -o cobertura.xml
+"""
 from lxml import etree
 
 from pyEDAA.UCIS.Cobertura import Coverage
