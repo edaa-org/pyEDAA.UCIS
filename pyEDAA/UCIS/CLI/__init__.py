558d1ee27810669a667d116797047182d886a92a
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-01-17 20:29:02 +0100

    Updated command in docs.

--- pyEDAA/UCIS/CLI/__init__.py
+++ pyEDAA/UCIS/CLI/__init__.py
@@ -46,7 +46,7 @@ At next use this layer's service program to convert from UCIS to Cobertura forma
 
 .. code-block::
 
-   python ucdb2cobertura.py -i ucdb.xml -o cobertura.xml
+   pyedaa-ucis --ucdb ucdb.xml --cobertura cobertura.xml
 """
 from argparse import RawDescriptionHelpFormatter
 from pathlib import Path
